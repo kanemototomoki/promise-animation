a5eb18a35117068914c241ffa6c282f590289ddd
kanemototomoki <kanemototomoki@gmail.com>
2020-05-28 23:29:04 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+
+const colorList = ['Aqua', 'DeepPink', 'Chartreuse', 'SkyBlue', 'LightSalmon'];
+const heightFromTopList = [20, 140, 260, 380, 500];
+
+const contents = Array.from(document.getElementsByClassName('content'));
+
+contents.forEach((val, i) => {
+  val.style.backgroundColor = colorList[i];
+  val.style.top = heightFromTopList[i] + 'px';
+});
+
+const delay = (time) => time * 1000;
+
+const delayPromises = (delayTime) => {
+  const promises = contents.map((val) => {
+    return () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          val.classList.add('slide-animation');
+          resolve();
+        }, delay(delayTime));
+      }).catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+    };
+  });
+
+  const sequentialProcess = promises.reduce((pre, cur) => {
+    return pre.then(cur);
+  }, Promise.resolve());
+
+  return sequentialProcess;
+};
+
+const btn = document.getElementById('js-btn');
+btn.addEventListener('click', () => {
+  const inputVal = document.getElementById('js-input').value;
+  delayPromises(inputVal);
+});
